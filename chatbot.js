e974eb357561e7ec9c42324764627f83ec12fcfa
max sami <skull037037@gmail.com>
2016-09-20 09:19:24 -0700

    Sept 20
    added some responces

--- chatbot.js
+++ chatbot.js
@@ -1,3 +1,42 @@
+  var Errors = 0;
+
+
 function Speak(){
-  $("#chat-area").prepend("SQUIGLY!");
+//echo input
+  $("#chat-area").append($("#input").val() + "<br>");
+
+if ($("#input").val() == "what is your name?"){
+    $("#chat-area").append("squigly" + "<br>");
+}
+else if($("#input").val() == "what time is it?"){
+  var d = new Date();
+
+  var chanceOfJoke = Math.floor((Math.random() * 24) + 1);
+
+  if( chanceOfJoke == 12){
+    $("#chat-area").append("it is currently high noon somewhere in the world but for you its " + d +"<br>");
+  }
+  else{
+    $("#chat-area").append("it is currently" + d +"<br>");
+  }
+}
+else if($("#input").val() == "how old are you?"){
+    $("#chat-area").append("I was born on September 19th");
+}
+else{
+  if(Errors == 0){
+      $("#chat-area").append("try asking something else" + "<br>");
+      Errors++;
+    }
+    else if(Errors == 1){
+        $("#chat-area").append("?" + "<br>");
+              Errors++;
+      }
+      else if(Errors == 2){
+          $("#chat-area").append("uh try asking something else" + "<br>");
+                Errors = 0;
+        }
+
+}
+
 }
