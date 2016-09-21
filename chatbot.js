ccd596650f51186fec43a2d9195a979da7205439
max sami <skull037037@gmail.com>
2016-09-21 09:09:01 -0700

    sept 21
    squigly can now read uppercase and lowercase and punctuation does not
    matter

--- chatbot.js
+++ chatbot.js
@@ -1,14 +1,84 @@
   var Errors = 0;
 
+  var letterConversion = {
+	"A": "a",
+	"B": "b",
+	"C": "c",
+	"D": "d",
+	"E": "e",
+	"F": "f",
+	"G": "g",
+	"H": "h",
+	"I": "i",
+	"J": "j",
+  "K": "k",
+  "L": "l",
+  "M": "m",
+  "N": "n",
+  "O": "o",
+  "P": "p",
+  "Q": "q",
+  "R": "r",
+  "S": "s",
+  "T": "t",
+  "U": "u",
+  "V": "v",
+  "W": "w",
+  "X": "x",
+  "Y": "y",
+  "Z": "z",
+  "a": "a",
+	"b": "b",
+	"c": "c",
+	"d": "d",
+	"e": "e",
+	"f": "f",
+	"g": "g",
+	"h": "h",
+	"i": "i",
+	"j": "j",
+  "k": "k",
+  "l": "l",
+  "m": "m",
+  "n": "n",
+  "o": "o",
+  "p": "p",
+  "q": "q",
+  "r": "r",
+  "s": "s",
+  "t": "t",
+  "u": "u",
+  "v": "v",
+  "w": "w",
+  "x": "x",
+  "y": "y",
+  "z": "z",
+  "!": "",
+  "?": "",
+  ".": "",
+  " ": " ",
+}
+
 
 function Speak(){
+  var ChatInput = $("#input").val();
+  var converting;
+  var storage = "";
+  converting = ChatInput.split('');
+for( i =0; i < converting.length; i++){
+    storage += letterConversion[converting[i]];
+  }
+  console.log( storage );
+  ChatInput = storage;
+
 //echo input
   $("#chat-area").append($("#input").val() + "<br>");
 
-if ($("#input").val() == "what is your name?"){
+//various questions with answers
+if (ChatInput == "what is your name"){
     $("#chat-area").append("squigly" + "<br>");
 }
-else if($("#input").val() == "what time is it?"){
+else if(ChatInput == "what time is it"){
   var d = new Date();
 
   var chanceOfJoke = Math.floor((Math.random() * 24) + 1);
@@ -20,8 +90,11 @@ else if($("#input").val() == "what time is it?"){
     $("#chat-area").append("it is currently" + d +"<br>");
   }
 }
-else if($("#input").val() == "how old are you?"){
+else if( ChatInput == "how old are you"){
     $("#chat-area").append("I was born on September 19th");
+}
+else if(ChatInput == "hello" || ChatInput == "hi"){
+    $("#chat-area").append("hi <br>");
 }
 else{
   if(Errors == 0){
@@ -29,7 +102,7 @@ else{
       Errors++;
     }
     else if(Errors == 1){
-        $("#chat-area").append("?" + "<br>");
+        $("#chat-area").append("I dont understand" + "<br>");
               Errors++;
       }
       else if(Errors == 2){
